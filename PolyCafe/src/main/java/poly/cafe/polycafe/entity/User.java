a27ecbe68f5f423d879df28d838e6a1dddaf2fea
Admin <Admin@Admin-PC>
2025-05-28 21:50:00 +0700

    wrwgergerg

--- PolyCafe/src/main/java/poly/cafe/polycafe/entity/User.java
+++ PolyCafe/src/main/java/poly/cafe/polycafe/entity/User.java
@@ -1,28 +1,76 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package poly.cafe.polycafe.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 /**
  *
  * @author Admin
  */
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Data
 public class User {
     private String username;
     private String password;
     private boolean enabled;
     private String fullname;
-    @Builder.Default
-    private String photo = "photo.png";
+    private String photo;
     private boolean manager;
+
+    public static class Builder {
+        private String username;
+        private String password;
+        private boolean enabled;
+        private String fullname;
+        private String photo = "photo.png";
+        private boolean manager;
+
+        public Builder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder enabled(boolean enabled) {
+            this.enabled = enabled;
+            return this;
+        }
+
+        public Builder fullname(String fullname) {
+            this.fullname = fullname;
+            return this;
+        }
+
+        public Builder photo(String photo) {
+            this.photo = photo;
+            return this;
+        }
+
+        public Builder manager(boolean manager) {
+            this.manager = manager;
+            return this;
+        }
+
+        public User build() {
+            return new User(username, password, enabled, fullname, photo, manager);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public User(String username, String password, boolean enabled, String fullname, String photo, boolean manager) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.fullname = fullname;
+        this.photo = photo;
+        this.manager = manager;
+    }
+
+    // Getter/setter nếu cần...
 }
+
